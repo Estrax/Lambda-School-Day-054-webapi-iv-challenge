e79c3792dd05f96daa71c1369c182d98c2ce724b
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-21 15:38:52 -0400

    updated project to use more env variables

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 const express = require('express');
 const initMiddleware = require('./src/serverMiddleware');
 const router = require('./src/router');
+require('dotenv').config();
 
 const app = express();
 initMiddleware(app);
 app.use('/api', router);
 
 const PORT = process.env.PORT || 6500;
+const HOSTNAME = process.env.HOSTNAME || 'localhost';
 
 app.listen(PORT, (err) => {
     if(err) throw err;
-    console.log(`App running on port ${PORT}`);
+    console.log(`App running on ${HOSTNAME} on port ${PORT}`);
 });
